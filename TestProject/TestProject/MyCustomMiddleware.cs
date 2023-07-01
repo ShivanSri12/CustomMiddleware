62fe76b6287bb9ecd40076fe9844c0ce8aa29ad2
ShivanSri12 <Shivansri10@gmail.com>
2023-07-02 00:01:43 +0530

    First Commit
    First Commit

---
+++ TestProject/TestProject/MyCustomMiddleware.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TestProject
+{
+	public class MyCustomMiddleware : IMiddleware
+	{
+		public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+		{
+			await context.Response.WriteAsync("Incoming Request from Custom Middleware \n");
+            await next(context);
+			await context.Response.WriteAsync("Outgoing Response \n");
+        }
+
+	}
+}
+
---
+++ TestProject/TestProject/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using TestProject;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<MyCustomMiddleware>();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.Use(async (context, next) =>
+    {
+        await context.Response.WriteAsync("Use Middleware Incoming Request \n");
+        await next();
+        await context.Response.WriteAsync("Use Middleware Outgoing Response \n");
+    });
+
+    app.UseMiddleware<MyCustomMiddleware>();
+    app.Run(async context => {
+        await context.Response.WriteAsync("Run Middleware Component\n");
+    });
+
+}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
+
+
+
+
